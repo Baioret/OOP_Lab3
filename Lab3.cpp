42d93eec2e3171f82a0969b471a23f331942eafe
Violetta <ranreo.reo@gmail.com>
2021-11-27 14:18:34 +0500

    Abstract Factory

--- Lab3.cpp
+++ Lab3.cpp
@@ -2,62 +2,6 @@
 
 using namespace std;
 
-class IArray {
-public:
-	virtual void set_value(int index, float value) = 0;
-	virtual float get_value(int index) = 0;
-	virtual int get_lenght() = 0;
-};
-
-class Array : public IArray {
-public:
-	float data[10];
-	virtual void set_value(int index, float value) {
-		data[index] = value;
-	}
-	virtual float get_value(int index) {
-		return data[index];
-	}
-	virtual int get_lenght() {
-		return 10;
-	}
-};
-
-//-----------------------
-
-class IList {
-public:
-	virtual float get_value() = 0;
-	virtual void first() = 0;
-	virtual void next() = 0;
-	virtual bool isEOL() = 0;
-};
-
-class MyStorage : public IList {
-
-private:
-
-	float data[10];
-	int curr;
-
-public:
-
-	float get_value() {
-		return data[curr];
-	}
-	void first() {
-		curr = 0;
-	}
-	void next() {
-		curr++;
-	}
-	bool isEOL() {
-		return curr > 9;
-	}
-};
-
-//-----------------------
-
 class CBase {
 
 public:
@@ -78,18 +22,6 @@ public:
 
 	void classname() override {
 		cout << "CDesc_1::classname\n";
-	}
-};
-
-class CDesc_1_2 : public CDesc_1 {
-public:
-
-	CDesc_1_2() {
-		cout << "CDesc_1_2::CDesc_1_2()\n";
-	}
-
-	void classname() override {
-		cout << "CDesc_1_2::classname\n";
 	}
 };
 
@@ -132,10 +64,128 @@ public:
 	}
 };
 
+class CBaseFactory {
+public:
+	virtual CBase* createDesc() = 0;
+	virtual ~CBaseFactory() {}
+};
+
+class CDesc_1Factory : public CBaseFactory {
+public:
+	CDesc_1Factory() {
+		cout << "CDesc_1Factory::CDesc_1Factory()\n";
+	}
+	CBase* createDesc() override {
+		cout << "CDesc_1Factory::createDesc()\n";
+		return new CDesc_1();
+	}
+};
+
+class CDesc_2Factory : public CBaseFactory {
+public:
+	CDesc_2Factory() {
+		cout << "CDesc_2Factory::CDesc_2Factory()\n";
+	}
+	CBase* createDesc() override {
+		cout << "CDesc_2Factory::createDesc()\n";
+		return new CDesc_2();
+	}
+};
+
+class CDesc_3Factory : public CBaseFactory {
+public:
+	CDesc_3Factory() {
+		cout << "CDesc_3Factory::CDesc_3Factory()\n";
+	}
+	CBase* createDesc() override {
+		cout << "CDesc_3Factory::createDesc()\n";
+		return new CDesc_3();
+	}
+};
+
+class CDesc_4Factory : public CBaseFactory {
+public:
+	CDesc_4Factory() {
+		cout << "CDesc_4Factory::CDesc_4Factory()\n";
+	}
+	CBase* createDesc() override {
+		cout << "CDesc_4Factory::createDesc()\n";
+		return new CDesc_4();
+	}
+};
+
+//-----------------------
+
+class IList {
+public:
+	virtual void add(CBase* obj) = 0;
+	virtual CBase* get_value() = 0;
+	virtual void first() = 0;
+	virtual void next() = 0;
+	virtual bool isEOL() = 0;
+};
+
+class MyStorage : public IList {
+
+private:
+
+	CBase* data[10]{};
+	int curr{};
+
+public:
+
+	MyStorage() {
+		curr = 0;
+	}
+
+	void add(CBase* obj) {
+		data[curr] = obj;
+		curr++;
+	}
+
+	CBase* get_value() {
+		return data[curr];
+	}
+	void first() {
+		curr = 0;
+	}
+	void next() {
+		curr++;
+	}
+	bool isEOL() {
+		return curr > 9;
+	}
+};
+
+//-----------------------
+
 int main()
 {
-	
+	srand(time(0));
 
+	MyStorage* storage = new MyStorage();
+
+	for (int i = 0; i < 10; i++)
+	{
+
+		CBaseFactory* factory{};
+
+		if (rand() % 4 == 0)
+			factory = new CDesc_1Factory();
+		else if (rand() % 4 == 1)
+			factory = new CDesc_2Factory();
+		else if (rand() % 4 == 2)
+			factory = new CDesc_3Factory();
+		else
+			factory = new CDesc_4Factory();
+
+		storage->add(factory->createDesc());
+
+		cout << endl;
+	}
+
+	for (storage->first(); !storage->isEOL(); storage->next())
+		storage->get_value()->classname();
 
     return 0;
 }
