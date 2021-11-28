34ca6ee2e7d7400bc0ffff820100542c04a18eeb
Violetta <ranreo.reo@gmail.com>
2021-11-28 17:16:24 +0500

    Проработка методов хранилища

--- Lab3.cpp
+++ Lab3.cpp
@@ -69,7 +69,8 @@ public:
 class IList {
 public:
 	virtual void add(CBase* obj) = 0;
-	virtual CBase* get_value() = 0;
+	virtual void del(CBase* obj) = 0;
+	virtual CBase* getObject() = 0;
 	virtual void first() = 0;
 	virtual void next() = 0;
 	virtual bool isEOL() = 0;
@@ -79,32 +80,76 @@ class MyStorage : public IList {
 
 private:
 
-	CBase* data[10]{};
-	int curr{};
+	CBase** data{};
+	int curr = 0;
+	int size = 10;
+	int count = 0;
+
+	void resize() {
+
+		CBase** tmp = new CBase * [size + 1]();
+
+		for (int i = 0; i < size; i++)
+			tmp[i] = data[i];
+
+		data = tmp;
+
+		size++;
+	}
 
 public:
 
 	MyStorage() {
-		curr = 0;
+		data = new CBase * [size]();
+		this->first();
 	}
 
 	void add(CBase* obj) {
+
+		if (this->isEOL())
+		{
+			if (count < curr) {
+				for (int i = 0; i < size; i++)
+					if (data[i] == NULL) {
+						data[i] = obj;
+						count++;
+						return;
+					}
+			}
+
+			this->resize();
+		}
+
 		data[curr] = obj;
 		curr++;
+		count++;
 	}
 
-	CBase* get_value() {
+	void del(CBase* obj) {
+		delete obj;
+		data[curr] = NULL;
+		count--;
+	}
+
+	CBase* getObject() {
 		return data[curr];
 	}
+
 	void first() {
 		curr = 0;
 	}
+
 	void next() {
 		curr++;
 	}
+
 	bool isEOL() {
-		return curr > 9;
+		return curr > size - 1;
 	}
+
+	/*void getCount() {
+		cout << count << endl;
+	}*/
 };
 
 //-----------------------
@@ -115,8 +160,9 @@ int main()
 
 	MyStorage* storage = new MyStorage();
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 14; i++)
 	{
+
 		if (rand() % 4 == 0)
 			storage->add(new CDesc_1);
 		else if (rand() % 4 == 1)
@@ -127,10 +173,41 @@ int main()
 			storage->add(new CDesc_4);
 
 		cout << endl;
+		//storage->getCount();
 	}
 
 	for (storage->first(); !storage->isEOL(); storage->next())
-		storage->get_value()->classname();
+		if (storage->getObject() != NULL)
+		{
+			if (rand() % 2 == 0)
+				storage->getObject()->classname();
+			else
+				storage->del(storage->getObject());
+		}
+		else
+			cout << "NULL\n";
+
+	for (int i = 0; i < 3; i++)
+	{
+
+		if (rand() % 4 == 0)
+			storage->add(new CDesc_1);
+		else if (rand() % 4 == 1)
+			storage->add(new CDesc_2);
+		else if (rand() % 4 == 2)
+			storage->add(new CDesc_3);
+		else
+			storage->add(new CDesc_4);
+
+		cout << endl;
+		//storage->getCount();
+	}
+
+	for (storage->first(); !storage->isEOL(); storage->next())
+		if (storage->getObject() != NULL)
+			storage->getObject()->classname();
+		else
+			cout << "NULL\n";
 
 	return 0;
 }
